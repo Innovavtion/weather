76c79d874db3071b53bbc2f702d5c9c47b24d51e
Nikita <pozhidaevw@gmail.com>
2023-02-01 23:58:35 +0700

    update: test layout weather

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 function App() {
   const hello: string = 'Weather App';
-  return <h1 className="">{hello}</h1>;
+  return (
+    <div className="relative overflow-hidden flex justify-center items-center snap-x snap-mandatory min-h-screen">
+      <div className="container bg-blue-500 w-64 h-80 rounded-lg shadow-xl snap-always">
+        <h1>{hello}</h1>
+      </div>
+      <div className="container bg-blue-500 w-80 h-96 rounded-lg shadow-xl mx-8 snap-always">
+        <h1>{hello}</h1>
+      </div>
+      <div className="container bg-blue-500 w-64 h-80 rounded-lg shadow-xl snap-always">
+        <h1>{hello}</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
