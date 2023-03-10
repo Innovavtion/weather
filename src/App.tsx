d5d2e75fc743913a5e4ed425b68bf1a7a0e22d60
Nikita <pozhidaevw@gmail.com>
2023-03-10 16:58:15 +0700

    update: background cards, style

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ function App() {
       <h1 className="title-text">{hello}</h1>
       <div className="weather-card-container">
         <div className="weather-card left-card">
-          <Icons name="sun" color="#111827" size="70" className="icons-card" />
+          <Icons name="cloud-shtil" color="#ffffff" size="75" className="icons-card" />
         </div>
         <div className="weather-card weather-card-active active-day">
-          <Icons name="sun" color="#111827" size="85" className="icons-card" />
+          <Icons name="cloud-rain-storm" color="#ffffff" size="100" className="icons-card" />
         </div>
         <div className="weather-card right-card">
-          <Icons name="sun" color="#111827" size="70" className="icons-card" />
+          <Icons name="sun" color="#ffffff" size="75" className="icons-card" />
         </div>
       </div>
       <Icons name="setting" color="000000" size="64" className="icon-setting" />
