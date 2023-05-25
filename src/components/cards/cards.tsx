ac0a8cd6bf118c715bf286f602a6a9d7e327e5ef
Nikita <pozhidaevw@gmail.com>
2023-05-25 14:40:12 +0300

    update: cards -> problem size icons

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -9,22 +9,27 @@ interface CardProps {
 }
 
 function Card({ weather }: CardProps) {
+  // Кастыльное решение для размеров иконок
+  const iconSizeWeather: string = weather.position === 'weather-card-active' ? '115' : '85';
+  const iconSizeHumidity: string = weather.position === 'weather-card-active' ? '35' : '27';
+  const iconSizeWindSpeed: string = weather.position === 'weather-card-active' ? '38' : '30';
+
   return (
     <div className={`weather-card ${weather.position}`}>
       <div className="day">{weather.day}</div>
-      <Icons name={`${weather.weather}`} color="#ffffff" size="85" className="icons-card" />
+      <Icons name={`${weather.weather}`} color="#ffffff" size={iconSizeWeather} className="icons-card" />
       <div className="temperature">{weather.temperature}</div>
       <div className="description">{weather.description}</div>
       <div className="description-info">
         <div className="humidity">
-          <Icons name="humidity" color="#ffffff" size="27" className="mini-icons-card" />
+          <Icons name="humidity" color="#ffffff" size={iconSizeHumidity} className="mini-icons-card" />
           <div className="description-text">
             <div>{weather.humidity}</div>
             <div>Humidity</div>
           </div>
         </div>
         <div className="wind-speed">
-          <Icons name="wind" color="#ffffff" size="30" className="mini-icons-card" />
+          <Icons name="wind" color="#ffffff" size={iconSizeWindSpeed} className="mini-icons-card" />
           <div className="description-text">
             <div>{weather.windSpeed}</div>
             <div>Wind Speed</div>
