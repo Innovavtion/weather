ea6e01c0e4ac522f0448d9b435cb4ebcae467629
Nikita <pozhidaevw@gmail.com>
2023-06-01 23:48:12 +0700

    add: icons, close modal;

--- src/components/modal/modalLocation.tsx
+++ src/components/modal/modalLocation.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Icons from '../icons/icons';
+
 interface IModal {
   modal: boolean;
   setModal: any;
@@ -11,6 +13,9 @@ export default function modalLocation({ modal, setModal }: IModal) {
       <div className="fixed z-10 bg-black/50 top-0 left-0 right-0 bottom-0" onClick={() => setModal(!modal)} />
       <div className="w-[500px] z-10 p-7 bg-gray-900 absolute top-1/3 left-1/2 -translate-x-1/2 drop-shadow-lg rounded-lg">
         <div className="">
+          <button onClick={() => setModal(!modal)} className="button-icon icons-close-modal">
+            <Icons name="close" color="#ffffff" size="20" />
+          </button>
           <div className="flex justify-center text-white font-bold mb-5 text-xl">Настройки отображаемых городов</div>
           <div className="flex mb-5">
             <input className="w-full rounded px-3 py-1 border-0" placeholder="Введите название города" />
